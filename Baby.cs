94084ebeb52f232b7f2dccb54591898ab0c1d06f
batuhan-uzun <batuhanuzun2356@gmail.com>
2024-09-04 02:34:22 +0300

    Add project files.

---
+++ Baby.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstructorProject
+{
+    internal class Baby
+    {
+        public Baby() // Default Constructor with no parameters
+        {
+            BirthDate = DateTime.Now;
+            Console.WriteLine("Ingaaaa");
+        }
+
+        public Baby(string name, string surname) // Alternative Constructor that takes parameters
+        {
+            Name = name;
+            Surname = surname;
+            BirthDate = DateTime.Now;
+            Console.WriteLine("Ingaaaa");
+        }
+
+        public DateTime BirthDate { get; set; } // We get set the Birth Date, Name and Last Name properties
+        public string Name { get; set; }
+        public string Surname { get; set; }
+    }
+}
---
+++ Constructor.cs
@@ -0,0 +1,14 @@
+﻿using ConstructorProject;
+using System.Threading.Channels;
+
+Baby baby1 = new Baby(); // We create a baby1 object using the default Constructor
+Console.WriteLine($"{baby1.Name} {baby1.Surname} {baby1.BirthDate.ToString("dd.MM.yyyy HH:mm")}");
+
+
+Console.WriteLine("Lütfen bebeğin adını giriniz:");
+string name = Console.ReadLine();
+Console.WriteLine("Lütfen bebeğin soyadını giriniz:");
+string surname = Console.ReadLine();
+
+Baby baby2 = new Baby(name,surname); // We fill in the parameters of our alternative Constructor by taking name and surname information
+Console.WriteLine($"{baby2.Name} {baby2.Surname} {baby2.BirthDate.ToString("dd.MM.yyyy HH:mm")}");
